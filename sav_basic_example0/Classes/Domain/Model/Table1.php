458c5b7265da4a43ade7e766f85b26301dbe3984
yolf <yolf.typo3@orange.fr>
2017-05-19 18:52:57 +0200

    SAV Basic Example0 regenerated.

--- sav_basic_example0/Classes/Domain/Model/Table1.php
+++ sav_basic_example0/Classes/Domain/Model/Table1.php
@@ -4,7 +4,7 @@ namespace SAV\SavBasicExample0\Domain\Model;
 /**
 *  Copyright notice
 *
-*  (c) 2016 Laurent Foulloy <yolf.typo3@orange.fr>
+*  (c) 2017 Laurent Foulloy <yolf.typo3@orange.fr>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
