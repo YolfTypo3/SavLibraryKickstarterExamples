02da2934adcf278b52180155ea9b132d63ed50a2
yolf <yolf.typo3@orange.fr>
2019-07-17 11:35:03 +0200

    Extension obsolete

--- sav_filter_selectors/ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (! defined('TYPO3_MODE'))
-    die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Controller/SavFilterSelectorsController.php', '_pi1', 'list_type', 0);
-
-?>
